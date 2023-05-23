dc43126db9d4e1af3bedd0c9bec4d26fe5da325f
ahmet bulutluoz <abbulutluoz@gmail.com>
2023-05-23 19:50:04 +0200

    18.gun sonu

--- src/test/java/tests/day18_TestNGReports_ParalelCalistirma/C02_RaporluSoftAssert.java
+++ src/test/java/tests/day18_TestNGReports_ParalelCalistirma/C02_RaporluSoftAssert.java
@@ -1,8 +1,10 @@
 package tests.day18_TestNGReports_ParalelCalistirma;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utilities.Driver;
 
 public class C02_RaporluSoftAssert {
@@ -12,15 +14,33 @@ public class C02_RaporluSoftAssert {
         // 1. Test
         //            -  https://the-internet.herokuapp.com/javascript_alerts adresine gidin
         Driver.getDriver().get("https://the-internet.herokuapp.com/javascript_alerts");
+
         //            - 1.alert'e tiklayin
         Thread.sleep(3000);
-        Driver.getDriver().findElement(By.xpath("//*[text()='Click for JS Alert']")).click();
+        WebElement ilkAlertElementi = Driver.getDriver().findElement(By.xpath("//*[text()='Click for JS Alert']"));
+        ilkAlertElementi.click();
+
         //            -  Alert'deki yazinin "I am a JS Alert" oldugunu test edin
         Thread.sleep(3000);
+
         String expectedAlertYazisi = "I am a JS Alert";
         String actualAlertYazisi = Driver.getDriver().switchTo().alert().getText();
-        Assert.assertEquals(expectedAlertYazisi,actualAlertYazisi);
+
+        SoftAssert softAssert = new SoftAssert();
+        // Assert.assertEquals(expectedAlertYazisi,actualAlertYazisi);
+        softAssert.assertEquals(actualAlertYazisi,expectedAlertYazisi,"alertteki yazi belirlenenden farkli");
+
         //            -  OK tusuna basip alert'i kapatin
+
         Driver.getDriver().switchTo().alert().accept();
+
+        // allert'in kapatildigini test edin
+
+        // Assert.assertTrue(ilkAlertElementi.isEnabled());
+
+        softAssert.assertTrue(ilkAlertElementi.isEnabled(),"alert kapatilamadi");
+
+        softAssert.assertAll();
+        Driver.closeDriver();
     }
 }
